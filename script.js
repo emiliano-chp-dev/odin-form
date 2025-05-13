9a4dfce6bc69d5b9bc4efe12a05b088f680eab01
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-13 19:13:03 +0300

    Import DOM elements and store them in constants

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function main() {
-  //
+  // DOM elements
+  const formElement = document.querySelector('#signup-form');
+  const formSubmitButton = document.querySelector('.submit_form');
+  const passwordInput = document.querySelector('#user_password');
+  const confirmPasswordInput = document.querySelector('#confirm_user_password');
+  const emailInput = document.querySelector('#email_address');
 }
 
 main();
